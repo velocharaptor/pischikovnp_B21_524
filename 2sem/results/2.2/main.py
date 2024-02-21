39fbc4afb1b1a2628282b95fceff0625d41a627e
nilgorithm <nullgorithm@gmail.com>
2024-02-21 10:54:40 +0300

    rm comments trash

--- 2sem/results/2.2/main.py
+++ 2sem/results/2.2/main.py
@@ -10,8 +10,8 @@ def cheating_otsu(input_path: str, output_path:str) -> None:
     image = cv2.imread(input_path, cv2.IMREAD_GRAYSCALE)
 
     # Применение CLAHE
-    clahe = cv2.createCLAHE(clipLimit=3, tileGridSize=(20,20))
-    image = clahe.apply(image)
+    # clahe = cv2.createCLAHE(clipLimit=3, tileGridSize=(20,20))
+    # image = clahe.apply(image)
 
     # Применение глобальной бинаризации с критерием Отсу
     _, binary_image = cv2.threshold(image, 0, 255, cv2.THRESH_OTSU)
@@ -29,35 +29,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# import cv2
-# from glob import glob
-# import os
-
-
-# def cheating_otsu(input_path: str, output_path: str) -> None:
-#     """В рамках курса я буду использовать 
-#     в т.ч. методы бинаризации как модули 
-#     из сторонникх библиотек. Вам так делать нельзя!"""
-#     image = cv2.imread(input_path, cv2.IMREAD_GRAYSCALE)
-
-#     # Применение CLAHE
-#     # clahe = cv2.createCLAHE(clipLimit=3, tileGridSize=(20,20))
-#     # image = clahe.apply(image)
-
-#     # Применение глобальной бинаризации с критерием Отсу
-#     _, binary_image = cv2.threshold(image, 0, 255, cv2.THRESH_OTSU)
-
-#     cv2.imwrite(output_path, binary_image)
-
-
-# def main():
-#     current_dir = os.path.dirname(os.path.abspath(__file__))
-#     output_path = os.path.join(current_dir, 'output')
-#     relative_path = "2sem/results/2.2/input/*"
-#     # relative_path = "2sem/results/input"
-#     for input_path in glob(relative_path):
-#         cheating_otsu(input_path, output_path)
-
-# if __name__ == "__main__":
-#     main()
